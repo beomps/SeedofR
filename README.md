d917556796a3c9e246865bf453e8e2c6bbf5fd52
beomps <beomps@gmail.com>
2019-09-26 11:04:35 +0900

    Add Watson Visual API

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SeedofR
 SmartFarm with Computer Vision and Machine Learning
+
+
+- Install
+pip install --upgrade "watson-developer-cloud>=2.4.1"
--- seedofr/views.py
+++ seedofr/views.py
@@ -3,13 +3,28 @@ from .models import Data
 from django.http import HttpResponse
 from .forms import PostForm
 from django.utils import timezone
-
+from watson_developer_cloud import VisualRecognitionV3
+import json
 
 def index(request):
     return render(request, 'seedofr/index.html')
 
 # 아래에 api 입력 및 로직 구현합니다.
 def post_new(request):
+
+    visual_recognition = VisualRecognitionV3(
+        '2018-03-19',
+        iam_apikey='XPidzig_QHPkej4yOM1_558gdA-uU6J-C0P8mPdKQB9c'
+    )
+
+    with open('./fruitbowl.jpg', 'rb') as images_file:
+        classes = visual_recognition.classify(
+            images_file,
+            threshold='0.6',
+        classifier_ids='default').get_result()
+    
+    print(json.dumps(classes, indent=2))        
+    
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
